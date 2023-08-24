df20fc5eb86ca9b2353ec8658035b7b6c8c9744e
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-24 11:03:57 -0500

    nav bar complete

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
@@ -9,39 +9,43 @@ function NavTabs() {
     <ul className="nav nav-tabs">
       <li className="nav-item">
         <Link
-          to="/"
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
           to="/About"
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === "/" ? "nav-link active" : "nav-link"}
         >
           About
         </Link>
       </li>
       <li className="nav-item">
         <Link
-          to="/Blog"
+          to="/Profile"
           // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Blog' ? 'nav-link active' : 'nav-link'}
+          className={
+            currentPage === "/Profile" ? "nav-link active" : "nav-link"
+          }
         >
-          Blog
+          Profolio
         </Link>
       </li>
       <li className="nav-item">
         <Link
           to="/Contact"
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+          className={
+            currentPage === "/Contact" ? "nav-link active" : "nav-link"
+          }
         >
           Contact
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Resume"
+          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
+          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
+          className={currentPage === "/Resume" ? "nav-link active" : "nav-link"}
+        >
+          Resume
         </Link>
       </li>
     </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,48 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App';
-import Error from './pages/Error';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import About from './pages/About';
-import Blog from './pages/Blog';
+import App from "./App";
+import Error from "./pages/Error";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Resume from "./pages/Resume";
+import Home from "./pages/Home";
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <Error />,
     children: [
+      {
+        path: "/About",
+        element: <About />,
+      },
       {
         index: true,
         element: <Home />,
       },
       {
-        path: '/About',
-        element: <About />,
+        path: "/Profile",
+        element: <Profile />,
       },
       {
-        path: '/Blog',
-        element: <Blog />,
-      },
-      {
-        path: '/Contact',
+        path: "/Contact",
         element: <Contact />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
 );
