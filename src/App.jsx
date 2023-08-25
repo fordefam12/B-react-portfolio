e58c1aed88b0507156d78ee451708ba62f65cd30
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-08-25 10:28:13 -0500

    footer established

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import Footer from './components/footer';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-    <div className='inline'>
+    <div className='col inline'>
     <h1 className='bg-primary'>Brandons porfolio</h1>
       <Nav />
-      <main className="mx-3">
+      <main className="col mx-3">
         <Outlet />
       </main>
+      <Footer></Footer>
       </div>
     </>
   );
---
+++ src/components/footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { VscGithubAlt } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+// import { Row } from "react-bootstrap";
+function Footer() {
+  const socialLinks = [
+    { url: "https://github.com/fordefam12", icon: <VscGithubAlt /> },
+    { url: "https://www.linkedin.com/in/fordefam12/", icon: <AiOutlineLinkedin /> },
+    { url: "https://www.instagram.com/fordefam12/", icon: <FaInstagram /> },
+  ];
+
+  return (
+    <footer class="text-center">
+      <h2>Made by Brandon Forde</h2>
+      <ul className="social-icons ">
+        {socialLinks.map((link, index) => (
+          <a class='pl-3 m-2' key={index} className="logo">
+            <a href={link.url} target="_blank" rel="noopener noreferrer">
+              {link.icon}
+            </a>
+          </a>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
